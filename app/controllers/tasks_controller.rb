0f2341d637b2f402cd2a0d5c6b154f93aae7ca0c
pearlery <natashaa.ler@gmail.com>
2024-10-31 00:56:47 +0700

    Todos App

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,39 +7,63 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-
     if @task.save
-      redirect_to tasks_url
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append("tasks_list", partial: "tasks/task", locals: { task: @task })
+        end
+        format.html { redirect_to tasks_url }
+      end
     else
       render :index, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.remove(@task)
+      end
+      format.html { redirect_to tasks_url }
     end
   end
 
   def toggle
     @task = Task.find(params[:id])
     @task.update(completed: !@task.completed)
-    render json: { message: "Success", completed: @task.completed }
+  
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(@task, partial: "tasks/task", locals: { task: @task })
+      end
+      format.html { redirect_to tasks_url }
+    end
   end
   
   def edit
     @task = Task.find(params[:id])
+    @categories = Category.all # Add this line to load categories
   end
+  
 
   def update
     @task = Task.find(params[:id])
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to tasks_url}
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(@task, partial: "tasks/task", locals: { task: @task })
+        end
+        format.html { redirect_to tasks_url, notice: "Task updated successfully" }
       else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(@task, partial: "tasks/form", locals: { task: @task })
+        end
         format.html { render :edit, status: :unprocessable_entity }
       end
     end
-  end
-
-  def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-    redirect_to tasks_url
   end
   
   
